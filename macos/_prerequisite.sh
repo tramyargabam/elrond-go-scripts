b0571093983fb1fe7d578844ed57324cf01590b9
Valentin Lup <valentin.lup@itnt.ro>
2019-10-29 01:09:00 +0200

    Initial release of macos install & launch scripts

---
+++ macos/_prerequisite.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#install some dependencies
+which -s brew
+if [[ $? != 0 ]] ; then
+    # Install Homebrew
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+  echo "Brew is already installed on this system !"
+    brew update
+fi
+
+brew install git go
+ 
